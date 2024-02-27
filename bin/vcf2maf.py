bbglab/deepCSA

commit 3a63cebf08dad5e7c6a9cb292315d25cf269473b

fix error in annotation processing
error appears when there are alternative haplotypes in the tsv because of the underscore separator

modules/local/summarize_annotation/main.nf
@@ -30,7 +30,7 @@ process SUMMARIZE_ANNOTATION {
             zgrep -v '#' \$file >> ${prefix}.vep.tab.tmp;
             echo \$file;
     done;
-    cat header.tsv <(sort -u ${prefix}.vep.tab.tmp | grep -v '#') > ${prefix}.vep.tab ;
+    cat header.tsv <(sort -u ${prefix}.vep.tab.tmp | grep -v '#' | sed 's/^chr//g' | awk -F ':' 'length(\$1) <= 2' | awk '{ printf "chr"\$0 }') > ${prefix}.vep.tab ;
     rm ${prefix}.vep.tab.tmp;
 
     postprocessing_annotation.py ${prefix}.vep.tab ${prefix}.vep.summary.tab ${assembly} False;

commit c28b2cce2eb6fe832104ee66737cdb694c2a18d3

fix bug in summarize annotation changes
- fix bug introduced in 3a63cebf08dad5e7c6a9cb292315d25cf269473b
This was putting all the variants in a single line
- add TODO in vcf2maf script
- add confidence level information from config file

bin/vcf2maf.py
@@ -13,7 +13,10 @@ from utils import vartype
 vcf = sys.argv[1]
 
 sampleid = sys.argv[2]
+
+# TODO remove project_name variable
 project_name = sys.argv[3]
+
 level = sys.argv[4]
 
 annotation_file = sys.argv[5]

conf/modules.config
@@ -226,6 +226,10 @@ process {
     }
 
 
+    withName: 'VCF2MAF' {
+        ext.level   = "${params.confidence_level}"
+    }
+
     withName: 'FILTERBATCH|MERGEBATCH|FILTERPANEL|VCF2MAF|COMPUTE_MUTABILITY|MATRIX_CONCAT|COMPUTETRINUC|POSTPROCESSVEPPANEL' {
         publishDir       = [
                 enabled : false

modules/local/summarize_annotation/main.nf
@@ -30,7 +30,7 @@ process SUMMARIZE_ANNOTATION {
             zgrep -v '#' \$file >> ${prefix}.vep.tab.tmp;
             echo \$file;
     done;
-    cat header.tsv <(sort -u ${prefix}.vep.tab.tmp | grep -v '#' | sed 's/^chr//g' | awk -F ':' 'length(\$1) <= 2' | awk '{ printf "chr"\$0 }') > ${prefix}.vep.tab ;
+    cat header.tsv <(sort -u ${prefix}.vep.tab.tmp | grep -v '#' | sed 's/^chr//g' | awk -F ':' 'length(\$1) <= 2' | awk '{ printf "chr"\$0"\\n" }') > ${prefix}.vep.tab ;
     rm ${prefix}.vep.tab.tmp;
 
     postprocessing_annotation.py ${prefix}.vep.tab ${prefix}.vep.summary.tab ${assembly} False;

nextflow.config
@@ -30,6 +30,8 @@ params {
     profileexons               = false
     profileintrons             = false
 
+    confidence_level           = 'high'
+
 
     // depth and panel
     min_depth                  = 10