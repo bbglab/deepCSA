ea57c494dd342ac5d2fe590ae408637c0b0be943
FerriolCalvet <ferriolcalvet@gmail.com>
2024-10-15 03:11:22 +0200

    fix bug in mutrate computation
    - final bug fix of the mutrate script:
       sample mut rate
       storing of proper columns

--- bin/compute_mutrate.py
+++ bin/compute_mutrate.py
@@ -123,15 +123,19 @@ def compute_mutrate(maf_path, depths_path, annot_panel_path, sample_name, panel_
     depths_df[sample_name.split('.')[0]] = depths_df[sample_name.split('.')[0]] / 3   # the depth per position can contribute to three different mutations
     depths_subset_adj_df = depths_df.merge(annot_panel_df[["CHROM", "POS", "GENE"]], on = ["CHROM", "POS"], how = "inner")
 
+    ## mode 3 (adjusted): each position counts as many times it contributes to the panel, but ONLY ONCE PER SAMPLE
+    depths_subset_adj_sample_df = depths_df.merge(annot_panel_df[["CHROM", "POS"]].drop_duplicates(subset = ["CHROM", "POS", "REF", "ALT"]),
+                                                    on = ["CHROM", "POS"], how = "inner")
+
     del depths_df
     del annot_panel_df
 
     # Compute mutation rates
     ## sample mutation rate
-    mutrate_sample_allmuts_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0])
-    mutrate_sample_snvs_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0], ["SNV"])
-    mutrate_sample_nonsnvs_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0], ["INSERTION", "DELETION", "COMPLEX", "MNV"])
-    mutrate_sample_indels_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0], ["INSERTION", "DELETION"])
+    mutrate_sample_allmuts_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_sample_df, sample_name.split('.')[0])
+    mutrate_sample_snvs_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_sample_df, sample_name.split('.')[0], ["SNV"])
+    mutrate_sample_nonsnvs_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_sample_df, sample_name.split('.')[0], ["INSERTION", "DELETION", "COMPLEX", "MNV"])
+    mutrate_sample_indels_df = mutrate_sample(maf_df, depths_subset_df, depths_subset_adj_sample_df, sample_name.split('.')[0], ["INSERTION", "DELETION"])
     ## per gene mutation rate
     mutrate_genes_allmuts_df = mutrate_gene(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0])
     mutrate_genes_snvs_df = mutrate_gene(maf_df, depths_subset_df, depths_subset_adj_df, sample_name.split('.')[0], ["SNV"])
@@ -145,9 +149,12 @@ def compute_mutrate(maf_path, depths_path, annot_panel_path, sample_name, panel_
 
     # Save
     mutrate_df[["SAMPLE_ID", "GENE", "REGIONS", "MUTTYPES",
-                "N_MUTS", "N_MUTATED", "DEPTH",
-                "MUTRATE_MB", "MUTRATE_MB_ADJUSTED", "MUTRATE_KB", "MUTREADSRATE_KB_ADJUSTED",
-                "MUTREADSRATE_MB", "MUTREADSRATE_MB_ADJUSTED", "MUTREADSRATE_KB", "MUTREADSRATE_KB_ADJUSTED"]].to_csv(f"{sample_name.split('.')[0]}.{panel_v}.mutrates.tsv",
+                "DEPTH",
+                "N_MUTS", "N_MUTATED",
+                "MUTRATE_MB", "MUTRATE_MB_ADJUSTED",
+                "MUTRATE_KB", "MUTRATE_KB_ADJUSTED",
+                "MUTREADSRATE_MB", "MUTREADSRATE_MB_ADJUSTED",
+                "MUTREADSRATE_KB", "MUTREADSRATE_KB_ADJUSTED"]].to_csv(f"{sample_name.split('.')[0]}.{panel_v}.mutrates.tsv",
                                                             sep = "\t",
                                                             header = True,
                                                             index = False
