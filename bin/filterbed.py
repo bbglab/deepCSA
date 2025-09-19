260d30266b59d98d68a4dfc8d86e5f8dd694b16b
Marta H <marta.huertas@irbbarcelona.org>
2025-09-19 12:41:17 +0200

    feature: add click to handle inputs
    These changes are copied from branch input-with-click,
    more specifically from commit 0af42a9d5e26fefa23c21e6f9ecd28e2386a0927.

--- bin/filterbed.py
+++ bin/filterbed.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-
-
-import sys
+import click
 import pandas as pd
 from utils import add_filter, to_int_if_possible
 from read_utils import custom_na_values
@@ -27,8 +25,6 @@ def negative_filter_panel_regions(mutations_df, positions_df, filtername):
     return mutations_df.drop("not_in_panel", axis = 1)
 
 
-
-
 def filter_panel_regions(mutations_df, positions_df, filtername):
     """
     Positive filter
@@ -48,57 +44,55 @@ def filter_panel_regions(mutations_df, positions_df, filtername):
 
 
 
-## TODO reimplement with click
-sample_maf_file = sys.argv[1]
-bedfile = sys.argv[2]
-filtername = sys.argv[3]
-positive = False
+@click.command()
+@click.option('--sample-maf-file', required=True, type=click.Path(exists=True), help='Input sample MAF file (TSV)')
+@click.option('--bedfile', required=True, type=click.Path(exists=True), help='Input BED file')
+@click.option('--filtername', required=True, type=str, help='Filter name to use')
+@click.option('--positive', is_flag=True, default=False, help='Use positive filter (default: negative filter)')
 
-sample_maf = pd.read_csv(sample_maf_file, sep = '\t', header = 0, na_values = custom_na_values)
+def main(sample_maf_file, bedfile, filtername, positive):
+    sample_maf = pd.read_csv(sample_maf_file, sep = '\t', header = 0, na_values = custom_na_values)
 
-current_filters = pd.unique(sample_maf["FILTER"].astype(str).str.split(";").explode())
+    current_filters = pd.unique(sample_maf["FILTER"].astype(str).str.split(";").explode())
+    if filtername in current_filters:
+        print("Not filtering with this BED file since the provided filter name is already present.")
+        exit(1)
 
-if filtername in current_filters:
-    print("Not filtering with this BED file since the provided filter name is already present.")
-    exit(1)
+    # read BED file
+    panel_reg = pd.read_csv(bedfile, sep = "\t", header = None)
+    # check if there is a header or not
+    first_coord = panel_reg.iloc[0,1]
+    if to_int_if_possible(first_coord):
+        panel_reg = panel_reg.iloc[:,:3]
+    # it means there is a header, and we don't want it
+    else:
+        panel_reg = panel_reg.iloc[1:,:3]
+    panel_reg.columns = ["CHROM", "START", "END"]
+    panel_reg["CHROM"] = panel_reg["CHROM"].astype(str)
+    panel_reg[["START", "END"]] = panel_reg[["START", "END"]].astype(int)
+    panel_reg["POS"] = [ list(range(x+1, y+1)) for x, y in panel_reg[["START", "END"]].values ]
+    positions_df = panel_reg.explode("POS").reset_index(drop = True)
+    positions_df = positions_df[["CHROM", "POS"]].drop_duplicates()
+    positions_df = positions_df.reset_index(drop = True)
 
+    # adjust the CHROM field to adapt to the way it is being represented in the mutations list
+    if sample_maf.iloc[0,0].startswith("chr") and not positions_df.iloc[0,0].startswith("chr"):
+        positions_df["CHROM"] = "chr" + positions_df["CHROM"]
+    elif not sample_maf.iloc[0,0].startswith("chr") and positions_df.iloc[0,0].startswith("chr"):
+        positions_df["CHROM"] = positions_df["CHROM"].str.replace("chr", "")
+    print("MAF")
+    print(sample_maf.head())
+    print("BED")
+    print(panel_reg.head())
+    if positive:
+        filtered_maf = filter_panel_regions(sample_maf, positions_df, filtername)
+    else:
+        filtered_maf = negative_filter_panel_regions(sample_maf, positions_df, filtername)
 
-# read BED file
-panel_reg = pd.read_csv(bedfile, sep = "\t", header = None)
+    filtered_maf.to_csv(f"{'.'.join(sample_maf_file.split('.')[:-2])}.filtered.tsv.gz",
+                                            sep = "\t",
+                                            header = True,
+                                            index = False)
 
-# check if there is a header or not
-first_coord = panel_reg.iloc[0,1]
-if to_int_if_possible(first_coord):
-    panel_reg = panel_reg.iloc[:,:3]
-
-# it means there is a header, and we don't want it
-else:
-    panel_reg = panel_reg.iloc[1:,:3]
-
-panel_reg.columns = ["CHROM", "START", "END"]
-panel_reg["CHROM"] = panel_reg["CHROM"].astype(str)
-panel_reg[["START", "END"]] = panel_reg[["START", "END"]].astype(int)
-
-
-panel_reg["POS"] = [ list(range(x+1, y+1)) for x, y in panel_reg[["START", "END"]].values ]
-positions_df = panel_reg.explode("POS").reset_index(drop = True)
-positions_df = positions_df[["CHROM", "POS"]].drop_duplicates()
-positions_df = positions_df.reset_index(drop = True)
-
-# adjust the CHROM field to adapt to the way it is being represented in the mutations list
-if sample_maf.iloc[0,0].startswith("chr") and not positions_df.iloc[0,0].startswith("chr"):
-    positions_df["CHROM"] = "chr" + positions_df["CHROM"]
-
-elif not sample_maf.iloc[0,0].startswith("chr") and positions_df.iloc[0,0].startswith("chr"):
-    positions_df["CHROM"] = positions_df["CHROM"].str.replace("chr", "")
-
-
-if positive:
-    filtered_maf = filter_panel_regions(sample_maf, positions_df, filtername)
-else:
-    filtered_maf = negative_filter_panel_regions(sample_maf, positions_df, filtername)
-
-filtered_maf.to_csv(f"{'.'.join(sample_maf_file.split('.')[:-2])}.filtered.tsv.gz",
-                                        sep = "\t",
-                                        header = True,
-                                        index = False)
+if __name__ == '__main__':
+    main()
