bdf142917630865a216a26b90c74d2e4ef036746
FerriolCalvet <ferriolcalvet@gmail.com>
2026-02-19 10:18:35 +0100

    minor update signature extraction script

--- assets/useful_scripts/signatures_sigprofilerextractor.py
+++ assets/useful_scripts/signatures_sigprofilerextractor.py
@@ -14,12 +14,13 @@ from SigProfilerExtractor import sigpro as sig
 @click.argument('output_dir')                     
 @click.argument('input')                           
 @click.option('--ref_genome', default='GRCh38', help='Reference genome to use') # When option parameter has to be specified in the command
+@click.option('--min_sig', default=1, help='Minimum number of signatures')
 @click.option('--max_sig', default=10, help='Maximum number of signatures')
 @click.option('--nmf_replicates', default=100, help='Number of NMF replicates')
 @click.option('--cpu', default=-1, help='Number of processors to be used to extract the signatures. Default value will use all available processors, which may cause a memory error.')
 
 # Run SigProfilerExtractor
-def main(input_type, output_dir, input, ref_genome, max_sig, nmf_replicates, cpu):
+def main(input_type, output_dir, input, ref_genome, min_sig, max_sig, nmf_replicates, cpu):
     """  
     Run SigProfilerExtractor with the specified parameters.  
     This script can be run using the deepCSA output available at: 
@@ -31,7 +32,8 @@ def main(input_type, output_dir, input, ref_genome, max_sig, nmf_replicates, cpu
         matrix \  
         ./output_dir \  
         count_matrix_wgs_20240821.txt \  
-        --ref_genome GRCh38 \  
+        --ref_genome GRCh38 \
+        --min_sig 1 \
         --max_sig 10 \  
         --nmf_replicates 100 \  
         --cpu 10  
@@ -41,7 +43,7 @@ def main(input_type, output_dir, input, ref_genome, max_sig, nmf_replicates, cpu
         output=output_dir,
         input_data=input, 
         reference_genome=ref_genome,
-        minimum_signatures=1,
+        minimum_signatures=min_sig,
         maximum_signatures=max_sig,
         nmf_replicates=nmf_replicates,
 	    cpu=cpu
